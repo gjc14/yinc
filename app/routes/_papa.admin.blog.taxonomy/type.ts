de59464089b981c0783daab8108a7d0fd713248a
gjc14 <yin@yinc.me>
2025-04-04 01:04:57 +0800

    feat: added optimistic taxonomy hooks

---
+++ app/routes/_papa.admin.blog.taxonomy/type.ts
@@ -0,0 +1,9 @@
+import { PostWithRelations } from '~/lib/db/post.server'
+import { useAdminBlogContext } from '~/routes/_papa.admin.blog/route'
+
+export type TagType = ReturnType<typeof useAdminBlogContext>['tags'][number] & {
+    posts: PostWithRelations[]
+}
+export type CategoryType = ReturnType<
+    typeof useAdminBlogContext
+>['categories'][number] & { posts: PostWithRelations[] }
---
+++ app/routes/_papa.admin.blog.taxonomy/utils.ts
@@ -0,0 +1,81 @@
+import { useFetchers } from '@remix-run/react'
+
+import { SubCategory } from '~/lib/db/schema'
+import { CategoryType, TagType } from './type'
+
+type pendingItem = ReturnType<typeof useFetchers>[number] & {
+    formData: FormData
+}
+
+/**
+ * Optimistic add with useSubmit/useFetchers
+ * @see https://www.youtube.com/watch?v=-0Qi0yMyLwQ
+ */
+const usePendingCategories = () => {
+    return useFetchers()
+        .filter((fetcher): fetcher is pendingItem => {
+            if (!fetcher.formData) return false
+            const gotchaTag = fetcher.formData.get('intent') === 'category'
+            const isPOST = fetcher.formMethod === 'POST'
+            return gotchaTag && isPOST
+        })
+        .map(fetcher => {
+            return {
+                id: Number(fetcher.formData?.get('id')),
+                name: fetcher.formData?.get('name'),
+                slug: fetcher.formData?.get('slug'),
+                description: fetcher.formData?.get('description'),
+                subCategories: [],
+                posts: [],
+            } as CategoryType
+        })
+}
+
+/**
+ * Optimistic add with useSubmit/useFetchers
+ * @see https://www.youtube.com/watch?v=-0Qi0yMyLwQ
+ */
+const usePendingSubCategories = () => {
+    return useFetchers()
+        .filter((fetcher): fetcher is pendingItem => {
+            if (!fetcher.formData) return false
+            const gotchaSubCategory =
+                fetcher.formData.get('intent') === 'subcategory'
+            const isPOST = fetcher.formMethod === 'POST'
+            return gotchaSubCategory && isPOST
+        })
+        .map(fetcher => {
+            return {
+                id: Number(fetcher.formData?.get('id')),
+                name: fetcher.formData?.get('name'),
+                categoryId: Number(fetcher.formData?.get('parentId')),
+                slug: fetcher.formData?.get('slug'),
+                description: fetcher.formData?.get('description'),
+            } as SubCategory
+        })
+}
+
+/**
+ * Optimistic add with useSubmit/useFetchers
+ * @see https://www.youtube.com/watch?v=-0Qi0yMyLwQ
+ */
+const usePendingTags = () => {
+    return useFetchers()
+        .filter((fetcher): fetcher is pendingItem => {
+            if (!fetcher.formData) return false
+            const gotchaTag = fetcher.formData.get('intent') === 'tag'
+            const isPOST = fetcher.formMethod === 'POST'
+            return gotchaTag && isPOST
+        })
+        .map(fetcher => {
+            return {
+                id: Number(fetcher.formData.get('id')),
+                name: String(fetcher.formData.get('name')),
+                slug: String(fetcher.formData.get('slug')),
+                description: String(fetcher.formData.get('description') || ''),
+                posts: [],
+            } as TagType
+        })
+}
+
+export { usePendingCategories, usePendingSubCategories, usePendingTags }
