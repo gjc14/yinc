1f74178e939c34386411fa9c8e9bbb2cb37792f4
gjc14 <yin@yinc.me>
2025-04-07 03:07:46 +0800

    feat: added email validation func

--- app/lib/utils/index.tsx
+++ app/lib/utils/index.tsx
@@ -88,3 +88,8 @@ export const generateBreadcrumbs = (pathname: string) => {
 
     return breadcrumbItems
 }
+
+export const isValidEmail = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return emailRegex.test(email)
+}
