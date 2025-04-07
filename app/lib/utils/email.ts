95686fab9340cc6770fdf60beb7ce3052a8d23c2
gjc14 <yin@yinc.me>
2025-04-07 23:56:56 +0800

    feat: email instance

---
+++ app/lib/utils/email.ts
@@ -0,0 +1,12 @@
+import { Resend } from 'resend'
+
+let emailInstance: Resend | null = null
+if (process.env.RESEND_API_KEY && process.env.AUTH_EMAIL) {
+    emailInstance = new Resend(process.env.RESEND_API_KEY)
+} else {
+    console.warn(
+        'RESEND_API_KEY and AUTH_EMAIL are required, email sending is now disabled'
+    )
+}
+
+export { emailInstance }
