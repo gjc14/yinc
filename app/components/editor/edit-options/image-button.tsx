f7fb5b4ee64c2afc23555a70a79e9523d2f1f932
gjc14 <yin@yinc.me>
2025-04-08 10:41:41 +0800

    docs: rename tailwindcss to app.css and tiptap image button

--- app/components/editor/edit-options/image-button.tsx
+++ app/components/editor/edit-options/image-button.tsx
@@ -87,7 +87,7 @@ export const ImageButton = ({ editor }: { editor: Editor }) => {
 									}
 								}}
 							>
-								Insert Youtube
+								Insert Image
 							</Button>
 						</PopoverClose>
 					</footer>
