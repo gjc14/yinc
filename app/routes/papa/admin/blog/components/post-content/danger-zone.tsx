21844bc48677c1be9a47c23c293800772173c331
gjc14 <yin@yinc.me>
2025-04-16 15:55:54 +0800

    style: update danger zone in post light mode

--- app/routes/papa/admin/blog/components/post-content/danger-zone.tsx
+++ app/routes/papa/admin/blog/components/post-content/danger-zone.tsx
@@ -21,16 +21,16 @@ export const DangerZone = ({
 		<>
 			<Separator />
 
-			<div className="w-full flex flex-col p-3 bg-destructive/70 border rounded-lg space-y-3">
+			<div className="w-full flex flex-col p-3 border rounded-lg space-y-3">
 				<h4>⚠️ Danger Zone</h4>
-				<div className="flex items-center justify-between border rounded-md py-1 px-2 bg-muted/60">
+				<div className="flex items-center justify-between border rounded-md py-1 px-2">
 					<div className="flex flex-col gap-0.5">
-						<p>Delete this post</p>
-						<p className="text-destructive-foreground/60">
-							This action cannot be undone.
-						</p>
+						<strong className="text-lg">Delete this post</strong>
+						<p className="">This action cannot be undone.</p>
 					</div>
-					<Button onClick={onDeleteRequest}>Delete Post</Button>
+					<Button onClick={onDeleteRequest} variant={'destructive'}>
+						Delete Post
+					</Button>
 				</div>
 			</div>
 		</>
