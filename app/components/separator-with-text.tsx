1339f8894a7f4a3bee7c1f5c73c379dd53e1eba1
gjc14 <yin@yinc.me>
2025-05-07 18:04:48 +0800

    style: separator with text remove takes in padding y

--- app/components/separator-with-text.tsx
+++ app/components/separator-with-text.tsx
@@ -5,21 +5,13 @@ import { cn } from '~/lib/utils'
  */
 const SeparatorWithText = ({
 	className,
-	paddingY = '12px',
 	text,
 }: {
 	className?: string
-	paddingY?: string
 	text: string
 }) => {
 	return (
-		<div
-			className={cn(
-				'flex items-center text-muted-foreground text-xs',
-				className,
-			)}
-			style={{ paddingTop: paddingY, paddingBottom: paddingY }}
-		>
+		<div className={cn('flex items-center text-muted text-xs py-1', className)}>
 			<hr className="grow border-current" />
 			<span className={'px-3'}>{text}</span>
 			<hr className="grow border-current" />
