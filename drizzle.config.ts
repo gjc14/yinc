adc90220c04aebf25a368b287512b7b9a8a37a2a
gjc14 <yin@yinc.me>
2025-05-07 23:11:22 +0800

    feat: drizzle schema endpoint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	dialect: 'postgresql',
-	schema: './app/lib/db/schema',
+	schema: [
+		'./app/lib/db/schema/**/*',
+		'./app/routes/plugins/**/lib/db/schema/**/*',
+	],
 	dbCredentials: {
 		url: process.env.DATABASE_URL!,
 	},
