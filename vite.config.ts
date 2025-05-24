41f1d0918c12539db8540b2768c580f5cbf9f226
gjc14 <yin@yinc.me>
2025-05-25 00:01:24 +0800

    chore: added svgr package to turn svg to component

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
+	plugins: [reactRouter(), tsconfigPaths(), tailwindcss(), svgr()],
 	ssr: {
 		noExternal: ['lucide-react', 'react-dropzone'],
 	},
