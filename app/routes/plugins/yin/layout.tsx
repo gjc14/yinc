4120481e6f87e2117a13432cbf99449835eebe99
gjc14 <yin@yinc.me>
2025-05-25 01:33:16 +0800

    feat: new main index page

--- app/routes/plugins/yin/layout.tsx
+++ app/routes/plugins/yin/layout.tsx
@@ -2,18 +2,8 @@ import { Outlet } from 'react-router'
 
 export default function Layout() {
 	return (
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<a href="/">/</a>
-					</li>
-					<li>
-						<a href="/blog">Blog</a>
-					</li>
-				</ul>
-			</nav>
+		<main className="w-full h-svh bg-brand/35">
 			<Outlet />
-		</div>
+		</main>
 	)
 }
