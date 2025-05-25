e9167abe257347ad577da56432936beb86ead92b
gjc14 <yin@yinc.me>
2025-05-25 14:49:38 +0800

    refactor(root route config): moved hello-world

--- app/routes/web/routes.ts
+++ app/routes/web/routes.ts
@@ -16,30 +16,23 @@ import { blogRoute, indexRoute, splatRoute } from './papa.routes'
 // Configure your customized routes here
 const customizedRoutes = [
 	// Add your customized routes here
+	...prefix('/hello-world', [
+		layout('./routes/web/hello-world/layout.tsx', [
+			index('./routes/web/hello-world/index/route.tsx'),
+			route(':whateverParam', './routes/web/hello-world/param/route.tsx'),
+		]),
+		// This is the same as the above, but using the `route` function
+		// route('/hello-world/hello', './routes/web/hello-world/layout.tsx', [
+		// 	index('./routes/web/hello-world/index/route.tsx'),
+		// 	route(':whateverParam', './routes/web/hello-world/param/route.tsx'),
+		// ])
+	]),
 ] satisfies RouteConfig
 
 const systemRoutes = [
 	layout('./routes/web/layout.tsx', [
 		...(customizedRoutes.length === 0
-			? [
-					indexRoute(),
-					blogRoute(),
-					splatRoute(),
-					...prefix('/hello-world', [
-						layout('./routes/web/hello-world/layout.tsx', [
-							index('./routes/web/hello-world/index/route.tsx'),
-							route(
-								':whateverParam',
-								'./routes/web/hello-world/param/route.tsx',
-							),
-						]),
-						// This is the same as the above, but using the `route` function
-						// route('/hello-world/hello', './routes/web/hello-world/layout.tsx', [
-						// 	index('./routes/web/hello-world/index/route.tsx'),
-						// 	route(':whateverParam', './routes/web/hello-world/param/route.tsx'),
-						// ])
-					]),
-				]
+			? [indexRoute(), blogRoute(), splatRoute()]
 			: // Adding customized web routes
 				customizedRoutes),
 	]),
