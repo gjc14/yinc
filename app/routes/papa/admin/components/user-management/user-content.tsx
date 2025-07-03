557bfe6c6b0d57fa975eebeaee3d3cad8cd74abb
gjc14 <yin@yinc.me>
2025-07-04 01:01:07 +0800

    feat: user-content export bulk editable dialog

--- app/routes/papa/admin/components/user-management/user-content.tsx
+++ app/routes/papa/admin/components/user-management/user-content.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Form } from 'react-router'
 
 import { Loader2, Save } from 'lucide-react'
@@ -22,6 +23,7 @@ import {
 	SelectValue,
 } from '~/components/ui/select'
 import { user } from '~/lib/db/schema'
+import { capitalize } from '~/lib/utils'
 
 type User = typeof user.$inferSelect
 
@@ -52,6 +54,7 @@ export const UserContent = ({
 					id="user-content"
 					className="grid gap-4 py-4"
 					onSubmit={e => {
+						if (isSubmitting) return
 						e.preventDefault()
 						const formData = new FormData(e.currentTarget)
 
@@ -66,9 +69,7 @@ export const UserContent = ({
 				>
 					<input type="hidden" name="id" defaultValue={user.id} />
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="email" className="text-right">
-							Email
-						</Label>
+						<Label htmlFor="email">Email</Label>
 						<Input
 							id="email"
 							name="email"
@@ -77,91 +78,285 @@ export const UserContent = ({
 							placeholder="ur@e.mail"
 						/>
 					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="name" className="text-right">
-							Name
-						</Label>
-						<Input
-							id="name"
-							name="name"
-							defaultValue={user.name ?? undefined}
-							className="col-span-3"
-							placeholder="Your name"
-						/>
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="image" className="text-right">
-							Image
-						</Label>
-						<Input
-							id="image"
-							name="image"
-							defaultValue={user.image ?? undefined}
-							className="col-span-3"
-							placeholder="e.g. https://placecats.com/300/200"
-						/>
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="role" className="text-right">
-							Role
-						</Label>
-						<Select name="role" defaultValue={user.role ?? 'user'}>
-							<SelectTrigger className="col-span-3">
-								<SelectValue id="role" placeholder="what's your role?" />
-							</SelectTrigger>
-							<SelectContent>
-								{['user', 'admin'].map(role => (
-									<SelectItem key={role} value={role}>
-										{role}
-									</SelectItem>
-								))}
-							</SelectContent>
-						</Select>
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="emailVerified" className="text-right col-span-1">
-							Verified
-						</Label>
-						<Checkbox
-							id="emailVerified"
-							name="emailVerified"
-							defaultChecked={user.emailVerified}
-							className="col-span-3 ml-2"
-						/>
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="banned" className="text-right col-span-1">
-							Banned
-						</Label>
-						<Checkbox
-							id="banned"
-							name="banned"
-							defaultChecked={user.banned ?? false}
-							className="col-span-3 ml-2"
-						/>
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="banReason" className="text-right">
-							Ban reason
-						</Label>
-						<Input
-							id="banReason"
-							name="banReason"
-							defaultValue={user.banReason ?? undefined}
-							className="col-span-3"
-							placeholder="Why the user is banned?"
-						/>
-					</div>
+
+					<BulkEditableFields user={user} />
+
 					{/* TODO: Ban Expires */}
 				</Form>
 				<DialogFooter>
 					<Button form="user-content" type="submit" disabled={isSubmitting}>
-						{isSubmitting ? (
-							<Loader2 size={16} className="animate-spin" />
-						) : (
-							<Save size={16} />
-						)}
+						{isSubmitting && <Loader2 className="animate-spin" />}
 						Save changes
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
+
+interface BulkEditableFieldsProps {
+	user: Pick<
+		User,
+		'name' | 'image' | 'role' | 'emailVerified' | 'banned' | 'banReason'
+	>
+	isBulkEdit?: boolean
+}
+function BulkEditableFields({
+	user,
+	isBulkEdit = false,
+}: BulkEditableFieldsProps) {
+	const [enabledFields, setEnabledFields] = useState<Set<string>>(new Set())
+
+	const handleFieldToggle = (fieldName: string, enabled: boolean) => {
+		const newSet = new Set(enabledFields)
+		if (enabled) {
+			newSet.add(fieldName)
+		} else {
+			newSet.delete(fieldName)
+		}
+		setEnabledFields(newSet)
+	}
+
+	return (
+		<>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_name"
+							onCheckedChange={checked => handleFieldToggle('name', !!checked)}
+						/>
+					)}
+					<Label
+						htmlFor="name"
+						className={
+							isBulkEdit && !enabledFields.has('name')
+								? 'text-muted-foreground'
+								: ''
+						}
+						aria-disabled
+					>
+						Name
+					</Label>
+				</div>
+				<Input
+					id="name"
+					name="name"
+					defaultValue={user.name ?? undefined}
+					className="col-span-3"
+					placeholder="Your name"
+					disabled={isBulkEdit && !enabledFields.has('name')}
+				/>
+			</div>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_image"
+							onCheckedChange={checked => handleFieldToggle('image', !!checked)}
+						/>
+					)}
+					<Label
+						htmlFor="image"
+						className={
+							isBulkEdit && !enabledFields.has('image')
+								? 'text-muted-foreground'
+								: ''
+						}
+					>
+						Image
+					</Label>
+				</div>
+
+				<Input
+					id="image"
+					name="image"
+					defaultValue={user.image ?? undefined}
+					className="col-span-3"
+					placeholder="e.g. https://placecats.com/300/200"
+					disabled={isBulkEdit && !enabledFields.has('image')}
+				/>
+			</div>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_role"
+							onCheckedChange={checked => handleFieldToggle('role', !!checked)}
+						/>
+					)}
+					<Label
+						htmlFor="role"
+						className={
+							isBulkEdit && !enabledFields.has('role')
+								? 'text-muted-foreground'
+								: ''
+						}
+					>
+						Role
+					</Label>
+				</div>
+
+				<Select name="role" defaultValue={user.role ?? 'user'}>
+					<SelectTrigger
+						className="col-span-3"
+						disabled={isBulkEdit && !enabledFields.has('role')}
+					>
+						<SelectValue id="role" placeholder="what's your role?" />
+					</SelectTrigger>
+					<SelectContent>
+						{['user', 'admin'].map(role => (
+							<SelectItem key={role} value={role}>
+								{role}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			</div>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_emailVerified"
+							onCheckedChange={checked =>
+								handleFieldToggle('emailVerified', !!checked)
+							}
+						/>
+					)}
+					<Label
+						htmlFor="emailVerified"
+						className={
+							isBulkEdit && !enabledFields.has('emailVerified')
+								? 'text-muted-foreground'
+								: ''
+						}
+					>
+						Verified
+					</Label>
+				</div>
+
+				<Checkbox
+					id="emailVerified"
+					name="emailVerified"
+					defaultChecked={user.emailVerified}
+					className="col-span-3 ml-2"
+					disabled={isBulkEdit && !enabledFields.has('emailVerified')}
+				/>
+			</div>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_banned"
+							onCheckedChange={checked =>
+								handleFieldToggle('banned', !!checked)
+							}
+						/>
+					)}
+					<Label
+						htmlFor="banned"
+						className={
+							isBulkEdit && !enabledFields.has('banned')
+								? 'text-muted-foreground'
+								: ''
+						}
+					>
+						Banned
+					</Label>
+				</div>
+
+				<Checkbox
+					id="banned"
+					name="banned"
+					defaultChecked={user.banned ?? false}
+					className="col-span-3 ml-2"
+					disabled={isBulkEdit && !enabledFields.has('banned')}
+				/>
+			</div>
+			<div className="grid grid-cols-4 items-center gap-4">
+				<div className="flex items-center gap-2 col-span-1">
+					{isBulkEdit && (
+						<Checkbox
+							name="bulk_banReason"
+							onCheckedChange={checked =>
+								handleFieldToggle('banReason', !!checked)
+							}
+						/>
+					)}
+					<Label
+						htmlFor="banReason"
+						className={
+							isBulkEdit && !enabledFields.has('banReason')
+								? 'text-muted-foreground'
+								: ''
+						}
+					>
+						Ban reason
+					</Label>
+				</div>
+
+				<Input
+					id="banReason"
+					name="banReason"
+					defaultValue={user.banReason ?? undefined}
+					className="col-span-3"
+					placeholder="Why the user is banned?"
+					disabled={isBulkEdit && !enabledFields.has('banReason')}
+				/>
+			</div>
+		</>
+	)
+}
+
+type UserBulkEditDialogProps = BulkEditableFieldsProps & {
+	isSubmitting: boolean
+	onSubmit: (formData: FormData) => void
+	open: boolean
+	onOpenChange: React.Dispatch<React.SetStateAction<boolean>>
+	role: string
+}
+
+export const UserBulkEditDialog = ({
+	user,
+	open,
+	onOpenChange,
+	role,
+	onSubmit,
+	isSubmitting,
+}: UserBulkEditDialogProps) => {
+	return (
+		<Dialog open={open} onOpenChange={onOpenChange}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Bulk Edit {capitalize(role)}</DialogTitle>
+					<DialogDescription>
+						You can choose only to update some fields of the selected {role}.
+						Please note that changes will apply to all selected {role}.
+					</DialogDescription>
+				</DialogHeader>
+				<Form
+					id="update-user"
+					className="grid gap-4 py-4"
+					onSubmit={e => {
+						if (isSubmitting) return
+						e.preventDefault()
+						const formData = new FormData(e.currentTarget)
+
+						const checkboxFields = ['emailVerified', 'banned']
+						checkboxFields.forEach(field => {
+							const isChecked = formData.get(field) === 'on'
+							formData.set(field, isChecked.toString())
+						})
+
+						onSubmit(formData)
+					}}
+				>
+					<BulkEditableFields user={user} isBulkEdit />
+				</Form>
+				<DialogFooter>
+					<Button form="update-user" type="submit" disabled={isSubmitting}>
+						{isSubmitting && <Loader2 className="animate-spin" />}
+						Bulk Edit
 					</Button>
 				</DialogFooter>
 			</DialogContent>
