7d1700e68f55daa755e1e160b5562da564eb2afb
gjc14 <yin@yinc.me>
2025-07-04 01:00:35 +0800

    feat: user resource take in bulk

--- app/routes/papa/admin/user/resource.ts
+++ app/routes/papa/admin/user/resource.ts
@@ -1,3 +1,4 @@
+import assert from 'node:assert/strict'
 import { type ActionFunctionArgs } from 'react-router'
 
 import { createUpdateSchema } from 'drizzle-zod'
@@ -10,12 +11,29 @@ import { handleError } from '~/lib/utils/server'
 
 import { validateAdminSession } from '../../auth/utils'
 
-const userUpdateSchema = createUpdateSchema(user).required().omit({
+const userUpdateSchema = createUpdateSchema(user).omit({
+	id: true, // id will be checked separately, may be a string or comma-separated string of IDs
 	createdAt: true,
 	updatedAt: true,
 	banExpires: true,
 })
 
+/**
+ * Please pass in id as a string or comma-separated string of IDs.
+ * You could directly pass an array of IDs to submit / fetcher, react router will convert it to a string.
+ * ```ts
+ * import { useFetcher } from 'react-router'
+ *
+ * const fetcher = useFetcher()
+ * fetcher.submit({
+ *   id: ['1', '2', '3'], // or '1,2,3' or '1' if you want to delete a single user
+ * }, {
+ *   method: 'POST',
+ *   action: '/papa/admin/users/resource',
+ * })
+ * ```
+ * @returns
+ */
 export const action = async ({ request }: ActionFunctionArgs) => {
 	if (!['POST', 'PUT', 'DELETE'].includes(request.method)) {
 		return {
@@ -26,11 +44,9 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 	const adminSession = await validateAdminSession(request)
 
 	const formData = await request.formData()
-	const rawData = Object.fromEntries(formData)
-
 	const userData: Record<string, string | boolean | File> = {}
 
-	for (const [key, value] of Object.entries(rawData)) {
+	for (const [key, value] of formData.entries()) {
 		if (['emailVerified', 'banned'].includes(key)) {
 			userData[key] = value === 'true'
 		} else {
@@ -43,36 +59,54 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 			try {
 				const email = userData.email
 				const name = userData.name
+				const role = userData.role
 
-				if (
-					!email ||
-					typeof email !== 'string' ||
-					!isValidEmail(email) ||
-					typeof name !== 'string'
-				) {
-					throw new Error('Invalid email')
-				}
+				assert(
+					typeof name === 'string' &&
+						typeof email === 'string' &&
+						(role === 'admin' || role === 'user'),
+					'Invalid arguments',
+				)
+
+				assert(isValidEmail(email), 'Invalid email')
 
 				const { user } = await auth.api.createUser({
 					body: {
 						email,
 						name,
 						password: '',
-						role: 'user',
+						role,
+					},
+				})
+
+				await auth.api.sendVerificationEmail({
+					body: {
+						email: user.email,
+						callbackURL: role === 'admin' ? '/admin' : '/',
 					},
 				})
 
 				return {
-					msg: `User ${user.email} has created successfully`,
+					msg: `${role} ${user.email} has created successfully`,
 				} satisfies ConventionalActionResponse
 			} catch (error) {
 				return handleError(error, request)
 			}
 		case 'PUT':
 			try {
-				const user = userUpdateSchema.parse(userData)
+				let user = userUpdateSchema.parse(userData)
+				const userIds = userData.id
+
+				console.log('userData', userData)
+
+				assert(
+					typeof userIds === 'string' && userIds.split(',').length > 0,
+					'Invalid user ID',
+				)
+				const userIdArray = userIds.split(',')
+
 				const { user: userUpdated } = await updateUser({
-					id: user.id,
+					id: userIdArray,
 					data: {
 						email: user.email,
 						emailVerified: user.emailVerified,
@@ -84,23 +118,41 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 					},
 				})
 
+				assert(userUpdated.length > 0, 'User not found')
+
+				const userNames = userUpdated
+					.map(u => u.name || u.email || u.id)
+					.join(', ')
+
 				return {
-					msg:
-						'Success update ' + (userUpdated[0]?.name || userUpdated[0]?.email),
+					msg: `${userNames} updated successfully`,
 				} satisfies ConventionalActionResponse
 			} catch (error) {
 				return handleError(error, request)
 			}
 		case 'DELETE':
-			const userId = userData.id
-			if (typeof userId !== 'string') {
-				throw new Error('Invalid argument')
-			}
-
 			try {
-				const { user } = await deleteUser(userId)
+				const userIds = userData.id
+
+				assert(
+					typeof userIds === 'string' && userIds.split(',').length > 0,
+					'Invalid user ID',
+				)
+				const userIdArray = userIds.split(',')
+
+				assert(
+					!userIdArray.includes(adminSession.user.id),
+					'You cannot delete yourself! 😠',
+				)
+
+				const { user } = await deleteUser(userIdArray)
+
+				assert(user.length > 0, 'User not found')
+
+				const userNames = user.map(u => u.name || u.email || u.id).join(', ')
+
 				return {
-					msg: `${user[0]?.email} deleted successfully`,
+					msg: `${userNames} deleted successfully`,
 				} satisfies ConventionalActionResponse
 			} catch (error) {
 				return handleError(error, request)
