331846a24b23be7144f05a8407525eb2c0fa196e
gjc14 <yin@yinc.me>
2025-07-15 10:54:02 +0800

    style: update shadcn components

--- app/components/ui/checkbox.tsx
+++ app/components/ui/checkbox.tsx
@@ -1,47 +1,49 @@
 import * as React from 'react'
 
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
-import { CheckIcon } from '@radix-ui/react-icons'
+import { CheckIcon } from 'lucide-react'
 
-import { cn } from '~/lib/utils'
+import { cn } from '~/lib/utils/index'
 
-const Checkbox = React.forwardRef<
-	React.ComponentRef<typeof CheckboxPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & {
-		icon?: React.ReactNode
-		love?: boolean
-	}
->(({ className, icon, love, ...props }, ref) => (
-	<CheckboxPrimitive.Root
-		ref={ref}
-		className={cn(
-			'peer border-primary focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground h-4 w-4 shrink-0 border shadow-sm focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
-			className,
-		)}
-		{...props}
-	>
-		<CheckboxPrimitive.Indicator
-			className={cn('flex items-center justify-center text-current')}
-		>
-			{love ? (
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-3 w-3"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-				>
-					<path
-						fillRule="evenodd"
-						d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-						clipRule="evenodd"
-					/>
-				</svg>
-			) : (
-				icon || <CheckIcon className="h-4 w-4" />
+function Checkbox({
+	className,
+	...props
+}: React.ComponentProps<typeof CheckboxPrimitive.Root> & {
+	icon?: React.ReactNode
+	love?: boolean
+}) {
+	return (
+		<CheckboxPrimitive.Root
+			data-slot="checkbox"
+			className={cn(
+				'peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+				className,
 			)}
-		</CheckboxPrimitive.Indicator>
-	</CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+			{...props}
+		>
+			<CheckboxPrimitive.Indicator
+				data-slot="checkbox-indicator"
+				className="flex items-center justify-center text-current transition-none"
+			>
+				{props.love ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-3 w-3"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fillRule="evenodd"
+							d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				) : (
+					props.icon || <CheckIcon className="size-3.5" />
+				)}
+			</CheckboxPrimitive.Indicator>
+		</CheckboxPrimitive.Root>
+	)
+}
 
 export { Checkbox }
