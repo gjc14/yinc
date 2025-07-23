0c8aacfdc534425afeca03bfa2ce08ff3c1ecd3f
gjc14 <yin@yinc.me>
2025-07-24 02:26:29 +0800

    perf: dashboard blog layout cache and fetch parallel

--- app/routes/papa/dashboard/blog/layout.tsx
+++ app/routes/papa/dashboard/blog/layout.tsx
@@ -1,3 +1,4 @@
+import type { Route } from './+types/layout'
 import { Outlet } from 'react-router'
 
 import { getPosts } from '~/lib/db/post.server'
@@ -5,15 +6,38 @@ import { getCategories, getTags } from '~/lib/db/taxonomy.server'
 
 export const loader = async () => {
 	try {
-		const { posts } = await getPosts({ status: 'ALL' })
-		const { tags } = await getTags()
-		const { categories } = await getCategories()
-		return { posts, tags, categories }
+		console.time('blog-loader')
+		// TODO: Post fetching should be optimized
+		const [postsData, tagsData, categoriesData] = await Promise.all([
+			getPosts({ status: 'ALL' }),
+			getTags(),
+			getCategories(),
+		])
+		console.timeEnd('blog-loader')
+		return {
+			posts: postsData.posts,
+			tags: tagsData.tags,
+			categories: categoriesData.categories,
+		}
 	} catch (error) {
 		console.error(error)
 		return { posts: [], categories: [], tags: [] }
 	}
 }
+
+let cache: Awaited<ReturnType<typeof loader>>
+export const clientLoader = async ({
+	serverLoader,
+}: Route.ClientLoaderArgs) => {
+	if (cache) {
+		return cache
+	}
+
+	cache = await serverLoader()
+	return cache
+}
+
+clientLoader.hydrate = true
 
 export default function DashboardBlog() {
 	return <Outlet />
