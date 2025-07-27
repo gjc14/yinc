f62af1c7a2e43b40076f0dc268498fc384d9eb7d
gjc14 <yin@yinc.me>
2025-07-28 02:06:05 +0800

    feat: hasChanges context with serverPostAtom and editorContentAtom

--- app/routes/papa/dashboard/blog/context.ts
+++ app/routes/papa/dashboard/blog/context.ts
@@ -4,14 +4,27 @@ import { atom } from 'jotai'
 import type { PostWithRelations } from '~/lib/db/post.server'
 import type { Category, Tag } from '~/lib/db/schema'
 
-export const postAtom = atom<PostWithRelations>()
+import { areDifferentPosts } from './utils'
+
+export const serverPostAtom = atom<PostWithRelations>() // only for comparison
+export const postAtom = atom<PostWithRelations>() // actually used
 export const tagsAtom = atom<Tag[]>([])
 export const categoriesAtom = atom<Category[]>([])
 export const editorAtom = atom<Editor | null>(null)
+export const editorContentAtom = atom<string | null>(null)
 
 export const isSettingsOpenAtom = atom(false)
 
+/** @deprecated use {@link hasChangesAtom} instead */
 export const isDirtyAtom = atom(false)
+export const hasChangesAtom = atom(get => {
+	const [server, draft] = [get(serverPostAtom), get(postAtom)]
+	const editorContent = get(editorContentAtom)
+	if (!editorContent || !server || !draft) return false
+
+	if (server.content !== editorContent) return true
+	return areDifferentPosts(server, draft)
+})
 
 export const isRestoreAlertOpenAtom = atom(false)
 export const isResetAlertOpenAtom = atom(false)
