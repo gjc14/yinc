ad38756630b0e514d9035cc8a23282c7b2f3ed13
gjc14 <yin@yinc.me>
2025-07-29 04:17:44 +0800

    style: blog loading indicator

---
+++ app/routes/web/blog/components/blog-loading/index.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export const BlogLoading = () => {
+	return (
+		<div className="fixed inset-0 z-50 flex items-center justify-center">
+			<div className="spinner" />
+		</div>
+	)
+}
--- app/routes/web/blog/layout.tsx
+++ app/routes/web/blog/layout.tsx
@@ -1,11 +1,13 @@
 import type { Route } from './+types/layout'
-import { Outlet } from 'react-router'
+import { useEffect, useState } from 'react'
+import { Outlet, useNavigation } from 'react-router'
 
 import { getPostBySlug, getPosts } from '~/lib/db/post.server'
 import { getSEO } from '~/lib/db/seo.server'
 import { createMeta } from '~/lib/utils/seo'
 import { validateAdminSession } from '~/routes/papa/auth/utils'
 
+import { BlogLoading } from './components/blog-loading'
 import { CTA } from './components/cta'
 import { Footer } from './components/footer'
 import { Nav } from './components/nav'
@@ -90,12 +92,44 @@ export const clientLoader = async ({
 clientLoader.hydrate = true
 
 export default function Blog() {
+	const navigation = useNavigation()
+	const isLoading = navigation.state === 'loading'
+
+	const [showLoading, setShowLoading] = useState(false)
+
+	useEffect(() => {
+		let timeoutId: NodeJS.Timeout
+
+		if (isLoading) {
+			timeoutId = setTimeout(() => {
+				setShowLoading(true)
+			}, 30)
+		} else {
+			setShowLoading(false)
+		}
+
+		return () => {
+			if (timeoutId) {
+				clearTimeout(timeoutId)
+			}
+		}
+	}, [isLoading])
+
 	return (
-		<main className="flex min-h-svh w-full flex-1 flex-col">
-			<Nav />
-			<Outlet />
-			<CTA />
-			<Footer />
-		</main>
+		<div className="relative">
+			<main className="relative flex min-h-svh w-full flex-1 flex-col">
+				<Nav />
+				<Outlet />
+				<CTA />
+				<Footer />
+			</main>
+
+			{isLoading && showLoading && (
+				<>
+					<div className="fixed inset-0 z-10 bg-black/50" />
+					<BlogLoading />
+				</>
+			)}
+		</div>
 	)
 }
