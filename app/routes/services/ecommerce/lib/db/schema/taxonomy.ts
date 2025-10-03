afe413617c14261fd8bf3626f40f047f8d81a3cb
gjc14 <yin@yinc.me>
2025-10-03 13:48:19 +0800

    feat(ec/schema): create taxonomy db schema

---
+++ app/routes/services/ecommerce/lib/db/schema/taxonomy.ts
@@ -0,0 +1,116 @@
+import { relations, sql, type InferSelectModel } from 'drizzle-orm'
+import {
+	check,
+	foreignKey,
+	integer,
+	serial,
+	varchar,
+} from 'drizzle-orm/pg-core'
+
+import { pgTable } from '~/lib/db/schema/helpers'
+
+export type EcTag = InferSelectModel<typeof ecTag>
+
+export const ecTag = pgTable('ec_tag', {
+	id: serial('id').primaryKey(),
+	name: varchar('name', { length: 100 }).notNull(),
+	slug: varchar('slug', { length: 100 }).notNull().unique(),
+	description: varchar('description', { length: 255 }),
+	image: varchar('image'),
+})
+
+export type EcCategory = InferSelectModel<typeof ecCategory>
+
+export const ecCategory = pgTable(
+	'ec_category',
+	{
+		id: serial('id').primaryKey(),
+		name: varchar('name', { length: 100 }).notNull(),
+		slug: varchar('slug', { length: 100 }).notNull().unique(),
+		description: varchar('description', { length: 255 }),
+		parentId: integer('parent_id'),
+		image: varchar('image'),
+	},
+	table => [
+		// Prevent self-reference: a category cannot be its own parent
+		check('no_self_reference', sql`${table.id} != ${table.parentId}`),
+		foreignKey({
+			name: 'parent_category_fk',
+			columns: [table.parentId],
+			foreignColumns: [table.id],
+		}).onDelete('cascade'),
+	],
+)
+
+export type EcBrand = InferSelectModel<typeof ecBrand>
+
+export const ecBrand = pgTable(
+	'ec_brand',
+	{
+		id: serial('id').primaryKey(),
+		name: varchar('name', { length: 100 }).notNull(),
+		slug: varchar('slug', { length: 100 }).notNull().unique(),
+		description: varchar('description', { length: 255 }),
+		parentId: integer('parent_id'),
+		image: varchar('image'),
+	},
+	table => [
+		// Prevent self-reference: a brand cannot be its own parent
+		check('no_self_reference', sql`${table.id} != ${table.parentId}`),
+		foreignKey({
+			name: 'parent_brand_fk',
+			columns: [table.parentId],
+			foreignColumns: [table.id],
+		}).onDelete('cascade'),
+	],
+)
+
+export type EcAttribute = InferSelectModel<typeof ecAttribute>
+
+export const ecAttribute = pgTable('ec_attribute', {
+	id: serial('id').primaryKey(),
+	name: varchar('name', { length: 100 }).notNull(),
+	slug: varchar('slug', { length: 100 }).notNull().unique(),
+	value: varchar('value', { length: 255 }),
+})
+
+// === Relations ===
+
+// import {
+// 	productToAttribute,
+// 	productToBrand,
+// 	productToCategory,
+// 	productToTag,
+// } from './product'
+
+// export const ecTagRelations = relations(ecTag, ({ many }) => ({
+// 	productToTag: many(productToTag),
+// }))
+
+// export const ecCategoryRelations = relations(ecCategory, ({ one, many }) => ({
+// 	productToCategory: many(productToCategory),
+// 	parent: one(ecCategory, {
+// 		fields: [ecCategory.parentId],
+// 		references: [ecCategory.id],
+// 		relationName: 'parent_child',
+// 	}),
+// 	children: many(ecCategory, {
+// 		relationName: 'parent_child',
+// 	}),
+// }))
+
+// export const ecBrandRelations = relations(ecBrand, ({ one, many }) => ({
+// 	productToBrand: many(productToBrand),
+// 	parent: one(ecBrand, {
+// 		fields: [ecBrand.parentId],
+// 		references: [ecBrand.id],
+// 		relationName: 'parent_child',
+// 	}),
+// 	children: many(ecBrand, {
+// 		relationName: 'parent_child',
+// 	}),
+// }))
+
+// export const ecAttributeRelations = relations(ecAttribute, ({ many }) => ({
+// 	productToAttribute: many(productToAttribute),
+// }))
