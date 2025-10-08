b6e3ce5f3e1e3314e27d3e22c7c28080a1381cd6
gjc14 <yin@yinc.me>
2025-10-09 01:49:58 +0800

    feat(ec/server): getProduct, getProductGallery, getCrossSellProducts

--- app/routes/services/ecommerce/lib/db/product.server.ts
+++ app/routes/services/ecommerce/lib/db/product.server.ts
@@ -6,13 +6,16 @@ import { convertDateFields } from '~/lib/db/utils'
 import { dbStore } from './db.server'
 import {
 	product,
+	productAttribute,
+	productCrossSell,
 	productOption,
 	productToBrand,
 	productToCategory,
 	productToTag,
+	productVariant,
 	type ProductStatus,
 } from './schema/product'
-import { ecBrand, ecCategory, ecTag } from './schema/taxonomy'
+import { ecAttribute, ecBrand, ecCategory, ecTag } from './schema/taxonomy'
 
 type Product = typeof product.$inferSelect
 type ProductOption = typeof productOption.$inferSelect
@@ -170,4 +173,220 @@ export async function getProducts({
 	})
 
 	return validProducts
+}
+
+export type ProductWithOption = Product & {
+	option: ProductOption
+}
+
+/** Single product select attribute type */
+export type ProductVariant = typeof productVariant.$inferSelect & {
+	option: typeof productOption.$inferSelect
+}
+
+/** Single product select attribute type */
+export type ProductAttribute = Omit<
+	typeof productAttribute.$inferSelect,
+	'productId' | 'attributeId'
+>
+
+/**
+ * Fetch a single product by its slug from the database, including its option and related taxonomy.
+ * @param slug - The slug of the product to fetch.
+ * @param preview - If true, fetch the product regardless of its status. Defaults to false.
+ * @returns The product with its option and related taxonomy, or null if not found.
+ */
+export const getProduct = async ({
+	slug,
+	preview = false,
+	status = 'PUBLISHED',
+}: {
+	slug: string
+	preview?: boolean
+	status?: ProductStatus
+}) => {
+	console.time('getProduct')
+	const products = await dbStore.execute<
+		ProductWithOption & {
+			categories: (typeof ecCategory.$inferSelect)[]
+			tags: (typeof ecTag.$inferSelect)[]
+			brands: (typeof ecBrand.$inferSelect)[]
+			variants: ProductVariant[]
+			attributes: ProductAttribute[]
+		}
+	>(sql`
+		SELECT
+		DISTINCT ON (p.id)
+			p.*,
+			CASE 
+                WHEN po.id IS NOT NULL 
+                THEN row_to_json(po.*) 
+                ELSE NULL 
+            END AS option,
+			COALESCE(
+				(
+					SELECT json_agg(c)
+					FROM ${productToCategory} pc
+					LEFT JOIN ${ecCategory} c ON pc.category_id = c.id
+					WHERE pc.product_id = p.id
+				),
+				'[]'::json
+			) AS categories,
+			COALESCE(
+				(
+					SELECT json_agg(t)
+					FROM ${productToTag} pt
+					LEFT JOIN ${ecTag} t ON pt.tag_id = t.id
+					WHERE pt.product_id = p.id
+				),
+				'[]'::json
+			) AS tags,
+			COALESCE(
+				(
+					SELECT json_agg(b)
+					FROM ${productToBrand} pb
+					LEFT JOIN ${ecBrand} b ON pb.brand_id = b.id
+					WHERE pb.product_id = p.id
+				),
+				'[]'::json
+			) AS brands,
+			COALESCE(
+				(
+					SELECT json_agg(
+						json_build_object(
+							'id', pv.id,
+							'product_id', pv.product_id,
+							'option_id', pv.option_id,
+							'combination', pv.combination,
+							'order', pv.order,
+							'option', row_to_json(po.*)
+						) ORDER BY pv.order ASC
+					)
+					FROM ${productVariant} pv
+					LEFT JOIN ${productOption} po ON pv.option_id = po.id
+					WHERE pv.product_id = p.id
+				),
+				'[]'::json
+			) AS variants,
+			COALESCE(
+				(
+					SELECT json_agg(
+						json_build_object(
+							'id', pa.id,
+							'name', CASE 
+								WHEN pa.attribute_id IS NOT NULL AND a.name IS NOT NULL 
+								THEN a.name 
+								ELSE pa.name 
+							END,
+							'value', CASE 
+								WHEN pa.attribute_id IS NOT NULL AND a.value IS NOT NULL 
+								THEN a.value 
+								ELSE pa.value 
+							END,
+							'order', pa.order,
+							'select_type', pa.select_type,
+							'visible', pa.visible
+						) ORDER BY pa.order ASC
+					)
+					FROM ${productAttribute} pa
+					LEFT JOIN ${ecAttribute} a ON pa.attribute_id = a.id
+					WHERE pa.product_id = p.id
+				),
+				'[]'::json
+			) AS attributes
+
+		FROM ${product} p
+		LEFT JOIN ${productOption} po ON p.product_option_id = po.id
+
+		LEFT JOIN ${productToTag} ptt ON p.id = ptt.product_id
+		LEFT JOIN ${ecTag} et ON ptt.tag_id = et.id
+
+		LEFT JOIN ${productToCategory} ptc ON p.id = ptc.product_id
+		LEFT JOIN ${ecCategory} ec ON ptc.category_id = ec.id
+
+		LEFT JOIN ${productToBrand} ptb ON p.id = ptb.product_id
+		LEFT JOIN ${ecBrand} eb ON ptb.brand_id = eb.id
+
+		LEFT JOIN ${productAttribute} pta ON p.id = pta.product_id
+		LEFT JOIN ${ecAttribute} ea ON pta.attribute_id = ea.id
+
+		WHERE
+			p.slug = ${slug}
+			AND ${!preview ? sql`p.status = ${status}` : sql`TRUE`}
+	`)
+	console.timeEnd('getProduct')
+
+	const pLength = products.rows.length
+
+	if (pLength === 0) return null
+
+	if (pLength === 1) {
+		const p = products.rows[0]
+
+		if (!p.option) {
+			console.error(`Product ${p.id} has no productOption`)
+			return null
+		}
+
+		return convertDateFields(
+			camelcaseKeys(p, { deep: true, stopPaths: ['variants.combination'] }),
+			[
+				'createdAt',
+				'updatedAt',
+				'deletedAt',
+				'publishedAt',
+				'saleStartsAt',
+				'saleEndsAt',
+			],
+		)
+	}
+
+	// Should never happen since slug is unique
+	console.error(`Expected 1 or 0 product, got ${pLength} from slug ${slug}`)
+	return null
+}
+
+export const getProductGallery = async (productId: number) => {
+	const gallery = await dbStore.query.productGallery.findMany({
+		where: (gallery, { eq }) => eq(gallery.productId, productId),
+	})
+	return gallery
+}
+
+export const getCrossSellProducts = async (
+	productId: number,
+): Promise<ProductListing[]> => {
+	const crossSells = await dbStore.execute<ProductListing>(sql`
+		SELECT
+		DISTINCT ON (p.id)
+			p.id,
+			p.name,
+			p.slug,
+			p.status,
+			p.updated_at,
+			CASE
+				WHEN po.id IS NOT NULL
+				THEN json_build_object(
+					'id', po.id,
+					'image', po.image,
+					'price', po.price,
+					'sale_price', po.sale_price,
+					'sku', po.sku,
+					'manage_stock', po.manage_stock,
+					'stock_status', po.stock_status
+				)
+				ELSE NULL
+	   		END AS option
+		FROM ${productCrossSell} pcs
+		LEFT JOIN ${product} p ON p.id = pcs.cross_sell_product_id
+		LEFT JOIN ${productOption} po ON p.product_option_id = po.id
+		WHERE pcs.product_id = ${productId}
+	`)
+
+	const converted = convertDateFields(
+		camelcaseKeys(crossSells.rows, { deep: true }),
+		['updatedAt'],
+	)
+
+	return converted
 }
