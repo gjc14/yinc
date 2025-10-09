7332dbb53f359dcdb00769db62e1369569a6d026
gjc14 <yin@yinc.me>
2025-10-09 21:40:56 +0800

    feat(ec/dashboard): product route loader getProduct to render <ProductEditPage />

--- app/routes/services/ecommerce/dashboard/02_products/02_product/page.tsx
+++ app/routes/services/ecommerce/dashboard/02_products/02_product/page.tsx
@@ -1,22 +1,37 @@
-import type {
-	ProductAttribute,
-	ProductVariant,
-	ProductWithOption,
-} from '../../../lib/db/product.server'
-import type { ecBrand, ecCategory, ecTag } from '../../../lib/db/schema'
+import { useIsMobile } from '~/hooks/use-mobile'
+import {
+	DashboardContent,
+	DashboardSectionWrapper,
+} from '~/routes/papa/dashboard/components/dashboard-wrapper'
+
+import type { getProduct, ProductListing } from '../../../lib/db/product.server'
+import type { productGallery } from '../../../lib/db/schema'
+import { Header } from '../../../store/layout/components/header'
+import { StoreProductPage } from '../../../store/product/page'
 
 type ProductEditPageProps = {
-	product:
-		| (ProductWithOption & {
-				categories: (typeof ecCategory.$inferSelect)[]
-				tags: (typeof ecTag.$inferSelect)[]
-				brands: (typeof ecBrand.$inferSelect)[]
-				variants: ProductVariant[]
-				attributes: ProductAttribute[]
-		  })
-		| null
+	product: NonNullable<Awaited<ReturnType<typeof getProduct>>>
+	productGalleryPromise: Promise<(typeof productGallery.$inferSelect)[]>
+	crossSellProductsPromise: Promise<ProductListing[]>
 }
 
-export function ProductEditPage({ product }: ProductEditPageProps) {
-	return <pre>{JSON.stringify({ product }, null, 2)}</pre>
+export function ProductEditPage(props: ProductEditPageProps) {
+	const isMobile = useIsMobile()
+
+	return (
+		<DashboardSectionWrapper>
+			<DashboardContent className="grid grid-cols-1 md:grid-cols-2">
+				{!isMobile && (
+					<section className="relative hidden overflow-scroll border md:block">
+						<Header />
+						<StoreProductPage {...props} />
+					</section>
+				)}
+
+				<section className="relative flex flex-col gap-2 overflow-scroll">
+					Edit Product Section
+				</section>
+			</DashboardContent>
+		</DashboardSectionWrapper>
+	)
 }
--- app/routes/services/ecommerce/dashboard/02_products/02_product/route.tsx
+++ app/routes/services/ecommerce/dashboard/02_products/02_product/route.tsx
@@ -1,6 +1,10 @@
 import type { Route } from './+types/route'
 
-import { getProduct } from '../../../lib/db/product.server'
+import {
+	getCrossSellProducts,
+	getProduct,
+	getProductGallery,
+} from '../../../lib/db/product.server'
 import { ProductEditPage } from './page'
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
@@ -8,7 +12,31 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
 		slug: params.productSlug,
 		edit: true,
 	})
-	return { product }
+
+	if (!product) {
+		throw new Response('Product Not Found', { status: 404 })
+	}
+
+	const productGalleryPromise = new Promise<
+		Awaited<ReturnType<typeof getProductGallery>>
+	>(async resolve => {
+		const gallery = await getProductGallery(product.id)
+		// Insert main product image as first image if exists
+		if (product.option.image) {
+			gallery.unshift({
+				image: product.option.image,
+				productId: product.id,
+				order: 0,
+				alt: product.name,
+				title: product.name,
+			})
+		}
+		resolve(gallery)
+	})
+
+	const crossSellProductsPromise = getCrossSellProducts(product.id)
+
+	return { product, productGalleryPromise, crossSellProductsPromise }
 }
 
 export default function ECProduct({ loaderData }: Route.ComponentProps) {
