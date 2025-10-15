02ae2c697f656b3d6fd017999fd0c2a3b874215c
gjc14 <yin@yinc.me>
2025-10-16 00:56:16 +0800

    feat(ui/MultiSelect): add controllable props

--- app/components/multi-select.tsx
+++ app/components/multi-select.tsx
@@ -3,25 +3,29 @@ import * as React from 'react'
 import { Command as CommandPrimitive, useCommandState } from 'cmdk'
 import { PlusCircle, TriangleAlert, X } from 'lucide-react'
 
-import { Badge } from '~/components/ui/badge'
-import {
-	Command,
-	CommandGroup,
-	CommandItem,
-	CommandList,
-} from '~/components/ui/command'
-import { cn } from '~/lib/utils'
+import { cn } from '../lib/utils'
+import { Badge } from './ui/badge'
+import { Command, CommandGroup, CommandItem, CommandList } from './ui/command'
+import { Spinner } from './ui/spinner'
 
 type Option = Record<'value' | 'label', string>
+type DisplayOption = Option & {
+	className?: string
+	style?: React.CSSProperties
+}
 
 interface BadgeProps {
-	option: Option
-	handleUnselect: (option: Option) => void
+	option: DisplayOption
+	handleUnselect: (option: DisplayOption) => void
 }
 
 const DefaultBadge = ({ option, handleUnselect }: BadgeProps) => {
 	return (
-		<Badge key={option.value} className="my-auto rounded-full">
+		<Badge
+			key={option.value}
+			className={cn('my-auto rounded-full', option.className)}
+			style={option.style}
+		>
 			{option.label}
 			<button
 				className="ring-offset-background focus:ring-ring ml-1.5 cursor-pointer rounded-full outline-hidden focus:ring-2"
@@ -37,11 +41,19 @@ interface MultiSelectInputProps {
 	/** Array of { value, label } to select from */
 	options?: Option[]
 	/** options selected by default */
-	defaultSelected?: Option[]
+	defaultSelected?: DisplayOption[]
 	/** Current selected options */
-	selected?: Option[]
+	selected?: DisplayOption[]
 	/** Callback function when the selected options change, returns current select state */
-	onSelectedChange?: (selected: Option[]) => void
+	onSelectedChange?: (selected: DisplayOption[]) => void
+	/** Input state */
+	input?: string
+	/** Input state */
+	setInput?: React.Dispatch<React.SetStateAction<string>>
+	/** When CommandList first triggered, useful for lazy load */
+	onInitSearch?: () => void
+	/** Display ui when is searching */
+	isSearching?: boolean
 	/** Callback function to create id for the new option created @default Math.random().toString(36).slice(2) */
 	createId?: (value: string) => string
 	/** Placeholder text for the input field */
@@ -50,70 +62,77 @@ interface MultiSelectInputProps {
 	className?: string
 	/** Custom badge component to render selected options */
 	badge?: (props: BadgeProps) => React.ReactNode
+	/** Error message state */
+	error?: string
+	/** Error message state */
+	setError?: React.Dispatch<React.SetStateAction<string>>
 }
 
 export const MultiSelect = (props: MultiSelectInputProps) => {
-	const internalMultiSelectRef = React.useRef<InternalMultiSelectRef>(null)
-
 	return (
-		<Command
-			onKeyDown={e => {
-				e.stopPropagation()
-				internalMultiSelectRef.current?.handleKeyDown(e)
-			}}
-			className="overflow-visible bg-transparent"
-		>
-			<InternalMultiSelect {...props} ref={internalMultiSelectRef} />
+		// Wrap in Command to use useCommandState
+		<Command className="overflow-visible bg-transparent">
+			<InternalMultiSelect {...props} />
 		</Command>
 	)
 }
 
-type InternalMultiSelectRef = {
-	handleKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void
-}
-
-const InternalMultiSelect = React.forwardRef<
-	InternalMultiSelectRef,
-	MultiSelectInputProps
->((props, ref) => {
+const InternalMultiSelect = (props: MultiSelectInputProps) => {
 	const {
 		options = [],
 		defaultSelected,
 		selected,
 		onSelectedChange,
-		createId = v => Math.random().toString(36).slice(2),
+		input,
+		setInput,
+		onInitSearch,
+		isSearching,
+		createId = v => `${Math.random().toString(36).slice(2)}-${v}`,
 		placeholder,
 		className,
 		badge: BadgeComponent = DefaultBadge,
+		error: ExternalError,
+		setError: ExternalSetError,
 	} = props
 
-	const count = useCommandState(s => s.filtered.count)
-	const [inputValue, setInputValue] = React.useState('')
-	const [isComposing, setIsComposing] = React.useState(false)
-	const [open, setOpen] = React.useState(false)
-	const [displayCreateButton, setDisplayCreateButton] = React.useState(false)
-	const [error, setError] = React.useState<string | null>(null)
+	const init = React.useRef(false)
+	const inputRef = React.useRef<HTMLInputElement>(null)
 
-	const [internalSelected, setInternalSelected] = React.useState<Option[]>(
-		defaultSelected ?? [],
+	const [internalInputValue, setInternalInputValue] = React.useState(
+		input ?? '',
 	)
+	const inputValue = input ?? internalInputValue
+	const setInputValue = setInput ?? setInternalInputValue
 
-	const isControlled = selected !== undefined
-	const currentSelected = isControlled ? selected : internalSelected
+	const [isComposing, setIsComposing] = React.useState(false)
+
+	const [open, setOpen] = React.useState(false)
+
+	const [internalError, internalSetError] = React.useState<string>(
+		ExternalError ?? '',
+	)
+	const error = ExternalError ?? internalError
+	const setError = ExternalSetError ?? internalSetError
+
+	const [internalSelected, setInternalSelected] = React.useState<
+		DisplayOption[]
+	>(defaultSelected ?? [])
+
+	const currentSelected = selected ?? internalSelected
 
 	const selectableOptions = options.filter(
 		o => !currentSelected.some(s => s.value === o.value),
 	)
 
-	const noOptions = count === 0
+	const noOptions = useCommandState(s => s.filtered.count) === 0
 
 	/** Helper function to update (un)controlled selected state */
 	const handleUpdate = React.useCallback(
 		(selected: Option[]) => {
 			onSelectedChange?.(selected)
-			!isControlled && setInternalSelected(selected)
+			setInternalSelected(selected)
 		},
-		[onSelectedChange, isControlled],
+		[onSelectedChange],
 	)
 
 	const handleUnselect = (option: Option) => {
@@ -127,57 +146,52 @@ const InternalMultiSelect = React.forwardRef<
 		)
 	}
 
-	/** Helper function to handle new option creation @returns boolean */
-	const createNewOption = React.useCallback(
-		(label: string) => {
-			if (!label.trim()) return false
-			const labelExists = optionLabelExists(label, currentSelected)
-			if (labelExists) {
-				setError('Label already exists')
-				return false
-			}
+	/** Helper function to handle new option creation @returns void */
+	const createNewOption = React.useCallback(() => {
+		if (!inputValue.trim()) return
 
-			const id = createId(label.trim())
-			const newOption: Option = {
-				value: id,
-				label: label.trim(),
-			}
-			handleUpdate([...currentSelected, newOption])
+		const labelExists = optionLabelExists(inputValue, currentSelected)
+		if (labelExists) {
+			return setError('Label already exists')
+		}
 
-			return true
-		},
-		[currentSelected, createId, handleUpdate],
-	)
+		const newOption: Option = {
+			value: createId(inputValue.trim()),
+			label: inputValue.trim(),
+		}
+
+		handleUpdate([...currentSelected, newOption])
+		setInputValue('')
+	}, [currentSelected, createId, handleUpdate])
 
 	const handleKeyDown = React.useCallback(
 		(e: React.KeyboardEvent<HTMLDivElement>) => {
-			setError(null)
-			if (isComposing) return
+			setError('')
+			if (isComposing) return e.stopPropagation()
 
 			if (e.key === 'Enter') {
-				// Create new option if inputValue is not empty and no options are available
-				if (inputValue && noOptions && createNewOption(inputValue)) {
-					// Clear input after creating new option
-					setInputValue('')
+				// Create new option if no options are available
+				if (noOptions) {
+					createNewOption()
+					return e.stopPropagation()
 				}
 			}
 			if (e.key === 'Delete' || e.key === 'Backspace') {
 				if (inputValue === '') {
 					handleUnselect(currentSelected[currentSelected.length - 1])
+					return e.stopPropagation()
 				}
 			}
 		},
-		[isComposing, inputValue, createNewOption, handleUnselect, currentSelected],
+		[
+			isComposing,
+			inputValue,
+			noOptions,
+			createNewOption,
+			handleUnselect,
+			currentSelected,
+		],
 	)
-
-	React.useImperativeHandle(ref, () => ({
-		handleKeyDown,
-	}))
-
-	const inputRef = React.useRef<HTMLInputElement>(null)
-	const handleContainerClick = () => {
-		inputRef.current?.focus()
-	}
 
 	return (
 		<>
@@ -186,7 +200,7 @@ const InternalMultiSelect = React.forwardRef<
 					'group border-input ring-offset-background focus-within:ring-ring focus-within:ring-offset cursor-text rounded-md border px-2 py-2 text-sm focus-within:ring-1',
 					className,
 				)}
-				onClick={handleContainerClick}
+				onClick={() => inputRef.current?.focus()}
 			>
 				<div className="flex flex-wrap items-center gap-1">
 					{currentSelected.map(option => (
@@ -199,8 +213,14 @@ const InternalMultiSelect = React.forwardRef<
 						ref={inputRef}
 						value={inputValue}
 						onValueChange={setInputValue}
+						onKeyDown={handleKeyDown}
 						onBlur={() => setOpen(false)}
-						onFocus={() => setOpen(true)}
+						onFocus={() => {
+							setOpen(true)
+
+							// Trigger onInitSearch only the first time CommandList is opened
+							!init.current && (init.current = true) && onInitSearch?.()
+						}}
 						onCompositionStart={() => {
 							setIsComposing(true)
 						}}
@@ -226,88 +246,92 @@ const InternalMultiSelect = React.forwardRef<
 						<div
 							className={`bg-popover text-popover-foreground animate-in absolute top-0 z-10 w-full rounded-md border shadow-md outline-hidden`}
 						>
-							{/* Rendered when count = 0 */}
-							{inputValue.trim() ? (
-								<CommandPrimitive.Empty className="p-1 text-sm">
-									<button
-										onMouseDown={e => {
-											e.preventDefault()
-											e.stopPropagation()
-										}}
-										onClick={() => {
-											if (createNewOption(inputValue)) {
-												setInputValue('')
-											}
-										}}
-										className="bg-accent flex w-full cursor-pointer items-center rounded-sm px-2 py-1.5 text-sm outline-hidden select-none"
-									>
-										<PlusCircle className="mr-2 h-4 w-4" />"{inputValue.trim()}"
-										<kbd className="ml-auto text-xs text-nowrap">return ⏎</kbd>
-									</button>
-								</CommandPrimitive.Empty>
+							{/* When searching */}
+							{isSearching ? (
+								<CommandGroup className="flex items-center justify-center py-2">
+									<Spinner />
+								</CommandGroup>
 							) : (
-								<CommandPrimitive.Empty className="py-6 text-center text-sm">
-									No options found...
-								</CommandPrimitive.Empty>
-							)}
-
-							<CommandGroup className={noOptions ? 'hidden' : ''}>
-								{error && (
-									<div className="mb-2 rounded-md border border-amber-500/50 px-2 py-1 text-amber-600">
-										<p className="text-sm">
-											<TriangleAlert
-												className="me-3 -mt-0.5 inline-flex opacity-60"
-												size={16}
-												aria-hidden="true"
-											/>
-											{error}
-										</p>
-									</div>
-								)}
-
-								{selectableOptions.map(option => (
-									<CommandItem
-										key={option.value}
-										onMouseDown={e => {
-											e.preventDefault()
-											e.stopPropagation()
-										}}
-										onSelect={() => {
-											handleUpdate([...currentSelected, option])
-											setInputValue('')
-										}}
-										className="cursor-pointer"
-									>
-										{option.label}
-									</CommandItem>
-								))}
-
-								{inputValue.trim() &&
-									!optionLabelExists(inputValue, selectableOptions) && (
-										<CommandItem
-											onMouseDown={e => {
-												e.preventDefault()
-												e.stopPropagation()
-											}}
-											onSelect={() => {
-												if (createNewOption(inputValue)) {
-													setInputValue('')
-												}
-											}}
-											className="cursor-pointer"
-										>
-											<PlusCircle className="mr-2 h-4 w-4" />"
-											{inputValue.trim()}"
-											<kbd className="ml-auto text-xs text-nowrap">
-												return ⏎
-											</kbd>
-										</CommandItem>
+								<>
+									{/* Rendered when count = 0 */}
+									{inputValue.trim() ? (
+										<CommandPrimitive.Empty className="p-1 text-sm">
+											<button
+												onMouseDown={e => {
+													e.preventDefault()
+													e.stopPropagation()
+												}}
+												onClick={createNewOption}
+												className="bg-accent flex w-full cursor-pointer items-center rounded-sm px-2 py-1.5 text-sm outline-hidden select-none"
+											>
+												<PlusCircle className="mr-2 h-4 w-4" />"
+												{inputValue.trim()}"
+												<kbd className="ml-auto text-xs text-nowrap">
+													return ⏎
+												</kbd>
+											</button>
+										</CommandPrimitive.Empty>
+									) : (
+										<CommandPrimitive.Empty className="py-6 text-center text-sm">
+											No options found...
+										</CommandPrimitive.Empty>
 									)}
-							</CommandGroup>
+
+									<CommandGroup className={noOptions ? 'hidden' : ''}>
+										{error && (
+											<div className="mb-2 rounded-md border border-amber-500/50 px-2 py-1 text-amber-600">
+												<p className="text-sm">
+													<TriangleAlert
+														className="me-3 -mt-0.5 inline-flex opacity-60"
+														size={16}
+														aria-hidden="true"
+													/>
+													{error}
+												</p>
+											</div>
+										)}
+
+										{selectableOptions.map(option => (
+											<CommandItem
+												key={option.value}
+												onMouseDown={e => {
+													e.preventDefault()
+													e.stopPropagation()
+												}}
+												onSelect={() => {
+													handleUpdate([...currentSelected, option])
+													setInputValue('')
+												}}
+												className="cursor-pointer"
+											>
+												{option.label}
+											</CommandItem>
+										))}
+
+										{inputValue.trim() &&
+											!optionLabelExists(inputValue, selectableOptions) && (
+												<CommandItem
+													onMouseDown={e => {
+														e.preventDefault()
+														e.stopPropagation()
+													}}
+													onSelect={createNewOption}
+													className="cursor-pointer"
+												>
+													<PlusCircle className="mr-2 h-4 w-4" />"
+													{inputValue.trim()}"
+													<kbd className="ml-auto text-xs text-nowrap">
+														return ⏎
+													</kbd>
+												</CommandItem>
+											)}
+									</CommandGroup>
+								</>
+							)}
 						</div>
 					</CommandList>
 				)}
 			</div>
 		</>
 	)
-})
+}
